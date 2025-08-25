7f79177e31002c7501890a8b49cbd846ab4fd37d
MaureenDadap <53959873+MaureenDadap@users.noreply.github.com>
2025-08-25 21:00:15 +0800

    feat: create csv.go to handle csv related operations

---
+++ csv.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"encoding/csv"
+	"io"
+	"os"
+)
+
+func ReadAndParseCSV(filename string) [][]string {
+	csvData := readCSVFile(filename)
+
+	data := parseCSV(csvData)
+
+	return data
+}
+
+// Read CSV file
+func readCSVFile(filename string) []byte {
+	f := openFile(filename)
+	defer f.Close()
+
+	return Must(io.ReadAll(f))
+}
+
+// Open a CSV File.
+// If the file does not exist, it will be created and the default headers will be added
+// This does not close the file returned, so it must be cleaned up by the function caller
+func openFile(filename string) *os.File {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		println("No CSV file, creating..")
+
+		file = Must(os.Create(filename))
+		addDefaultHeaders(file)
+	}
+
+	return file
+}
+
+// Writes default headers to the file
+func addDefaultHeaders(file io.Writer) {
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	CheckErr(writer.Write(getHeaders()))
+}
+
+// Get the expected header of the CSV file
+func getHeaders() []string {
+	return []string{"ID", "Name", "Created At", "Is Done"}
+}
+
+// Parses CSV and returns it as a matrix of strings
+func parseCSV(data []byte) [][]string {
+	reader := csv.NewReader(bytes.NewReader(data))
+
+	records := Must(reader.ReadAll())
+
+	return records
+}
